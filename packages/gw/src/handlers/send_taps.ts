9db8e968f1294ea6a8484ba29dfca14d5d151e35
Nikita Kuznetsov <n@8gen.team>
2024-10-13 13:50:52 +0200

    feat(gw): limit batch size to 100 items

--- packages/gw/src/handlers/send_taps.ts
+++ packages/gw/src/handlers/send_taps.ts
@@ -11,7 +11,8 @@ export const SchemaSendTaps = t.Array(
             y: t.Number({ minimum: 0, maximum: 20000, multipleOf: 1 }),
         },
         { additionalProperties: false }
-    )
+    ),
+    { minItems: 1, maxItems: 100 } // we expect that user can tap 10 times per second (batch) using 10 fingers
 );
 export type SendTapsRequest = Static<typeof SchemaSendTaps>;
 
