55b620e19baee53ff71ead7a464342fa52c40fa3
Nick <0xc0fe@protonmail.com>
2024-12-03 00:45:23 +0100

    feat: add prom

--- packages/tt/dev-run.sh
+++ packages/tt/dev-run.sh
@@ -1,8 +1,10 @@
 export APP_PASSWORD=123 REPLICATOR_PASSWORD=456 STORAGE_PASSWORD=789 
-tt stop cluster
+killall -9 tt tarantool
+tt stop cluster 
+
 rm -rf /opt/tt/var/
 tt start cluster
 sleep 1
 echo 'fixture()' | tt connect cluster:router-a-001
-tt log -n 20 cluster:router-a-001
+tt log -n 20 cluster
 tt connect cluster:router-a-001
--- packages/tt/instances.enabled/cluster/cluster-scm-1.rockspec
+++ packages/tt/instances.enabled/cluster/cluster-scm-1.rockspec
@@ -9,6 +9,7 @@ dependencies = {
     'vshard == 0.1.27',
     'crud == 1.5.2',
     'websocket == 0.0.2',
+    'metrics-export-role == 0.1.0-1',
 }
 build        = {
     type = 'none',
